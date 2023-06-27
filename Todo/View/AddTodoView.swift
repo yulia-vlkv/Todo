3b45eea8f13db89e0f93a3d97295254c1929addc
Iuliia Vlkv <yu.wlkw@gmail.com>
2023-06-27 21:32:08 +0500

    Created a new todo entity, added save function

--- Todo/View/AddTodoView.swift
+++ Todo/View/AddTodoView.swift
@@ -6,16 +6,22 @@
 //
 
 import SwiftUI
+import CoreData
 
 struct AddTodoView: View {
     // MARK: - Properties
     
+    @Environment(\.managedObjectContext) var viewContext  
     @Environment(\.presentationMode) var presentationMode
     
     @State private var name: String = ""
     @State private var priority: String = "Normal"
     
     let priorities = ["High", "Normal", "Low"]
+    
+    @State private var errorShowing: Bool = false
+    @State private var errorTitle: String = ""
+    @State private var errorMessage: String = ""
     
     // MARK: - Body
 
@@ -35,7 +41,24 @@ struct AddTodoView: View {
                     .pickerStyle(SegmentedPickerStyle())
                     // MARK: - Save button
                     Button(action: {
-                        print("Save a new todo item.")
+                        if self.name != "" {
+                            let todo = Todo(context: viewContext)
+                            todo.name = self.name
+                            todo.priority = self.priority
+                            do {
+                                try self.viewContext.save()
+                                print("NEW TODO: \(String(describing: todo.name))")
+                            } catch {
+                                let nsError = error as NSError
+                                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+                            }
+                        } else {
+                            self.errorShowing = true
+                            self.errorTitle = "Invalid name"
+                            self.errorMessage = "Make sure to enter something for\nthe new todo item."
+                            return
+                        }
+                        self.presentationMode.wrappedValue.dismiss()
                     }, label: {
                         Text("Save")
                     }) //: Save button 
@@ -49,6 +72,9 @@ struct AddTodoView: View {
             }, label: {
                 Image(systemName: "xmark")
             }))
+            .alert(isPresented: $errorShowing) {
+                Alert(title: Text(errorTitle), message: Text(errorMessage ), dismissButton: .default(Text("OK")))
+            }
         } //: Navigation
     }
 }
--- Todo/View/ContentView.swift
+++ Todo/View/ContentView.swift
@@ -14,20 +14,20 @@ struct ContentView: View {
     
     @Environment(\.managedObjectContext) private var viewContext
 
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
+    @FetchRequest(entity: Todo.entity(),
+        sortDescriptors: [NSSortDescriptor(keyPath: \Todo.name, ascending: true)],
         animation: .default)
-    private var items: FetchedResults<Item>
+    private var todos: FetchedResults<Todo>
     
     // MARK: - Body
     var body: some View {
         NavigationView {
             List {
-                ForEach(items) { item in
+                ForEach(todos) { todo in
                     NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
+                        Text("Item at \(todo.name!)")
                     } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
+                        Text(todo.name!)
                     }
                 }
                 .onDelete(perform: deleteItems)
@@ -44,7 +44,7 @@ struct ContentView: View {
                         Label("Add Item", systemImage: "plus")
                     } //: Add button
                     .sheet(isPresented: $showingAddTodoView) {
-                        AddTodoView()
+                        AddTodoView().environment(\.managedObjectContext, self.viewContext)
                     }
                 }
             }
@@ -52,25 +52,9 @@ struct ContentView: View {
         }
     } //: Navigation
 
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-
     private func deleteItems(offsets: IndexSet) {
         withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
+            offsets.map { todos[$0] }.forEach(viewContext.delete)
 
             do {
                 try viewContext.save()
@@ -94,6 +78,6 @@ private let itemFormatter: DateFormatter = {
 // MARK: - Preview
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+        ContentView().environment(\.managedObjectContext, PersistenceController.shared.container.viewContext)
     }
 }
